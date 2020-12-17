6100b9e338e388010804b8e4cb891cc0dd6d40b6
Tomohiro SAKAI <tomohiro.sakai.j7y@denso-wave.com>
2020-12-17 14:36:07 +0900

    case4の追加」

---
+++ case4/case04/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace case04
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowViewModel(this);
+        }
+
+    }
+}
